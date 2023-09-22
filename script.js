79392cfc5054158ef43a3172152c044080139163
David Forbes <davidforbesi@hotmail.com>
2023-09-22 15:39:54 +0100

    remvoved unessesary if statement from rotateArrow function

--- script.js
+++ script.js
@@ -4,11 +4,9 @@ navItem.forEach((item) => item.addEventListener("mouseenter", rotateArrow));
 navItem.forEach((item) => item.addEventListener("mouseleave", rotateArrow));
 
 function rotateArrow(e) {
-  if (e.target.classList.contains("nav-item")) {
-    const index = e.target.dataset.index;
-    const arrow = document.querySelector(`i.${index}`);
-    arrow.classList.toggle("rotate");
-    const menu = document.querySelector(`div.menu.${index}`);
-    menu.classList.toggle("show-hide");
-  }
+  const index = e.target.dataset.index;
+  const arrow = document.querySelector(`i.${index}`);
+  arrow.classList.toggle("rotate");
+  const menu = document.querySelector(`div.menu.${index}`);
+  menu.classList.toggle("show-hide");
 }
